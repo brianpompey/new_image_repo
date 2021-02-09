755301e6d604f7a499d1a4e6fb26f356bd95a6b6
brianpompey <brianthescholar@gmail.com>
2021-02-09 16:56:10 -0500

    pictures controller built

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,29 @@
+class PicturesController < ApplicationController
+  def index
+    @pictures = Picture.all
+  end
+
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
+  def new
+    @picture = Picture.new
+    #current user
+  end
+
+  def create
+  end
+
+  def destroy
+    @picture = Image.find(params[:id])
+    @picture.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:name, :description, :image)
+  end
+end
