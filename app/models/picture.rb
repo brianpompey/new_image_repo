df9a7583100530edf4ede7cc309d244bcb240cb3
brianpompey <brianthescholar@gmail.com>
2021-02-16 13:33:36 -0500

    images displaying fine on index page more seeds coming

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,4 @@
 class Picture < ApplicationRecord
-   # belongs_to :database
    # belongs_to :user
     has_one_attached :image
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'pictures/index'
+  root 'pictures#index'
   get 'pictures/show'
   get 'pictures/create'
   get 'pictures/destroy'
